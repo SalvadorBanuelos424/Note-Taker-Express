59a6bac0ba8dc6bf75d1b77ccd6bbd8bf064e896
Sal <salvador.banuelos.424@gmail.com>
2022-05-08 22:28:10 -0700

    POST function

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
 // Filesystem function node-module
 const fs = require('fs');
-
 // Back-end function node-module
 const express = require('express');
-
 // Routing function node-module
 const path = require('path');
-
+//  Access database
+const db = require('./db/db.json');
 // use port localhost3001 OR a container-based platform like HEROKU
 const PORT = process.env.PORT || 3001;
-
 // app function node-module through Express.js
 const app = express();
 
 
 // Middleware: parse incoming request Object into string & array
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 // Middleware: parse incoming json
 app.use(express.json());
 // Middleware: serves static files in dir/public
 app.use(express.static('public'));
-
-
-// absolute path directly to note.html with res.sendFile
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-// absolute path directly to index.html with res.sendFile
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-// Wildcard route absolute path directly to index.html..
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 
 // GET requests fetches data
@@ -64,6 +48,18 @@ app.post('/api/notes', (req, res) => {
 });
 
 
+// absolute path directly to note.html with res.sendFile
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+// absolute path directly to index.html with res.sendFile
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+// Wildcard route absolute path directly to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 // start server and listens on port 3001
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}');
